e441f1a8bf13be85008bafec91711a0eea2f0642
emifa <emifa@LAPTOP-1TM449QB>
2023-07-05 14:28:11 +0200

    Aggiungere i file di progetto.

---
+++ Esercizio-4-LINQ--.NET/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static void Main()
+    {
+      
+        List<int> numeriCasuali = GenerateRandomNumber(1000);
+
+      
+        var numeriOrdinati = numeriCasuali.OrderBy(n => n.ToString().Length);
+
+      
+        foreach (var numero in numeriOrdinati)
+        {
+            Console.WriteLine(numero);
+        }
+    }
+
+    static List<int> GenerateRandomNumber(int quantita)
+    {
+        Random random = new Random();
+        List<int> numeriCasuali = new List<int>();
+
+        for (int i = 0; i < quantita; i++)
+        {
+            numeriCasuali.Add(random.Next(1000));
+        }
+
+        return numeriCasuali;
+    }
+}
